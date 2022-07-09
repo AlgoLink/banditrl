ea10b60b3e7c2f8b37f7b18ee968b0d92f0ccd3e
leepand <leepand@leepanddeMacBook-Pro.local>
2022-07-09 20:15:17 +0800

    first commit

---
+++ banditrl/__init__.py
@@ -0,0 +1,15 @@
+"""
+banditrl
+========
+Available subpackages
+---------------------
+bandit
+    Implementation of bandit algorithms.
+storage
+    Interface for storing history.
+"""
+import pkg_resources
+
+
+__all__ = ['bandit', 'storage']
+__version__ = pkg_resources.get_distribution('banditrl').version
