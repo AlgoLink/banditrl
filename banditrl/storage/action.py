682bf911f1f9f8945ec473a98f8e02fc85aba511
leepand <leepand@leepanddeMacBook-Pro.local>
2022-07-11 13:43:27 +0800

    update action storage using rlite

--- banditrl/storage/action.py
+++ banditrl/storage/action.py
@@ -198,8 +198,6 @@ class RliteActionStorage(ActionStorage):
             self.rlite_client= hirlite.Rlite("online_rlite.db",encoding='utf8')
         else:
             self.rlite_client= hirlite.Rlite(rlite_path,encoding='utf8')
-        self._actions = {}
-        self._next_action_id = 0
         self._model_id = model_id
 
     def get(self, action_id,model_id=None):
@@ -215,9 +213,11 @@ class RliteActionStorage(ActionStorage):
         """
         if model_id is None:
             model_id = self._model_id
-        _action = self.rlite_client.command("get","_actions:{0}:{1}".format(action_id,model_id))
+        model_dict_key = "_actions:dict:{}".format(model_id)
+
+        _action = self.rlite_client.command("get",model_dict_key)
         if _action is not None:
-            return pickle.loads(_action)
+            return pickle.loads(_action)[action_id]
         return _action
 
 
@@ -257,11 +257,11 @@ class RliteActionStorage(ActionStorage):
             model_id = self._model_id
         model_items_key = "_actions:onlineitems:{}".format(model_id)
         model_dict_key = "_actions:dict:{}".format(model_id)
-        _actions=self.rlite_client.command('get',model_dict_key)
-        if _actions is None :
-            actions = {}
+        _actions_dict=self.rlite_client.command('get',model_dict_key)
+        if _actions_dict is None :
+            _actions = {}
         else:
-            actions = pickle.loads(_actions)
+            _actions = pickle.loads(_actions_dict)
         for action in actions:
             action_text = action.text
             action_id = action.id
@@ -270,22 +270,27 @@ class RliteActionStorage(ActionStorage):
             action_text_key = "_actions:{0}:{1}".format(action_text,model_id)
             _action = self.rlite_client.command("get",action_text_key)
             if _action is not None:
-                raise KeyError("Action id {} exists".format(pickle.loads(_action).id))
+                raise KeyError("Action text {} exists".format(pickle.loads(_action).text))
 
-            if action.id is None:
+            if action_id is None:
                 action.id = self.rlite_client.command("incr","_actions:{0}:ids".format(model_id))
-                
-            action_id_key = "_actions:{0}:{1}".format(action.id,model_id)
-            
+                action_id_key = "_actions:{0}:{1}".format(action.id,model_id)
+            else:
+                action_id_key = "_actions:{0}:{1}".format(action_id,model_id)
+                _action_id_content = self.rlite_client.command("get",action_id_key)
+                if _action_id_content is not None:
+                    raise KeyError("Action id {} exists".format(pickle.loads(_action_id_content).id))
+
             self.rlite_client.command("set",action_id_key,pickle.dumps(action))
             self.rlite_client.command("set",action_text_key,pickle.dumps(action))
             
-            self.rlite_client.command("lpush",model_items_key,str(action.id))
-            actions[action.id] = action
+            self.rlite_client.command("sadd",model_items_key,str(action.id))
+            _actions[action.id] = action
+            _actions[action.text] = action.type
 
-        self.rlite_client.command("set",model_dict_key,pickle.dumps(actions))
+        self.rlite_client.command("set",model_dict_key,pickle.dumps(_actions))
             
-        return self.rlite_client.command("lrange",model_items_key,"0","-1")
+        return self.rlite_client.command('SMEMBERS', model_items_key)
 
     def update(self, action,model_id=None):
         r"""Update action
@@ -300,13 +305,38 @@ class RliteActionStorage(ActionStorage):
         if model_id is None:
             model_id = self._model_id
         model_dict_key = "_actions:dict:{}".format(model_id)
+        action_text = action.text
+        action_id  = action.id
+        action_text_key = "_actions:{0}:{1}".format(action_text,model_id)
+        action_id_key = "_actions:{0}:{1}".format(action_id,model_id)
         _actions=self.rlite_client.command('get',model_dict_key)
         if _actions is None :
             actions = {}
+            actions[action.id] = action
+            self.rlite_client.command("set",model_dict_key,pickle.dumps(actions))
+            self.rlite_client.command("set",action_id_key,pickle.dumps(action))
+            self.rlite_client.command("set",action_text_key,pickle.dumps(action))
+            return True
         else:
             actions = pickle.loads(_actions)
-        actions[action.id] = action
-        self.rlite_client.command("set",model_dict_key,pickle.dumps(actions))
+            if action.text in actions:
+                action_text_key = "_actions:{0}:{1}".format(action.text,model_id)
+                _old_action = pickle.loads(self.rlite_client.command("get",action_text_key))
+                del actions[action.text]
+                del actions[_old_action.id]
+            actions[action.id] = action
+            actions[action.text] = action.type
+            self.rlite_client.command("set",model_dict_key,pickle.dumps(actions))
+            self.rlite_client.command("set",action_id_key,pickle.dumps(action))
+            self.rlite_client.command("set",action_text_key,pickle.dumps(action))
+            old_text = self.get_by_text(action.text)
+            if old_text is None:
+                self.rlite_client.command("set",action_text_key,pickle.dumps(action))
+            else:
+                _action_text_key = "_actions:{0}:{1}".format(old_text.text,model_id)
+                self.rlite_client.command('del', _action_text_key)
+                self.rlite_client.command("set",action_text_key,pickle.dumps(action))
+                return True
 
     def remove(self, action_id,model_id=None):
         r"""Remove action
@@ -324,10 +354,18 @@ class RliteActionStorage(ActionStorage):
         _actions=self.rlite_client.command('get',model_dict_key)
         if _actions is None :
             actions = {}
+            return
         else:
             actions = pickle.loads(_actions)
+            _text= actions[action_id].text
+            _action_text_key = "_actions:{0}:{1}".format(_text,model_id)
+            action_id_key = "_actions:{0}:{1}".format(action_id,model_id)
             del actions[action_id]
             self.rlite_client.command("set",model_dict_key,pickle.dumps(actions))
+            self.rlite_client.command('del', _action_text_key)
+            self.rlite_client.command('del', action_id_key)
+            return
+            
 
     def count(self,model_id=None):
         r"""Count actions
@@ -336,17 +374,46 @@ class RliteActionStorage(ActionStorage):
         count: int
             Number of Action in the storage.
         """
+        if model_id is None:
+            model_id = self._model_id
         model_items_key = "_actions:onlineitems:{}".format(model_id)
-        return len(list(set(rlite.command('lrange', model_items_key,"0","-1"))))
+        return len(self.rlite_client.command('SMEMBERS', model_items_key))
 
-    def iterids(self):
+    def iterids(self,model_id=None):
         r"""Return iterable of the Action ids.
         Returns
         -------
         action_ids: iterable
             Action ids.
         """
-        return six.viewkeys(self._actions)
+        if model_id is None:
+            model_id = self._model_id
+        model_dict_key = "_actions:dict:{}".format(model_id)
+        _actions=self.rlite_client.command('get',model_dict_key)
+        if _actions is None :
+            actions = {}
+        else:
+            actions = pickle.loads(_actions)
+        return six.viewkeys(actions)
 
-    def __iter__(self):
-        return iter(six.viewvalues(self._actions))
+    def __iter__(self,model_id=None):
+        if model_id is None:
+            model_id = self._model_id
+        model_dict_key = "_actions:dict:{}".format(model_id)
+        _actions=self.rlite_client.command('get',model_dict_key)
+        if _actions is None :
+            actions = {}
+        else:
+            actions = pickle.loads(_actions)
+        return iter(six.viewvalues(actions))
+    
+    @property
+    def actions(self):
+        """Get actions dict."""
+        model_dict_key = "_actions:dict:{}".format(self._model_id)
+        _actions=self.rlite_client.command('get',model_dict_key)
+        if _actions is None :
+            actions = {}
+        else:
+            actions = pickle.loads(_actions)
+        return actions
