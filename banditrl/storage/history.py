aac4f85a4389fc1cf4d4528130c76a8d0a3f12e7
leepand <leepand@leepanddeMacBook-Pro.local>
2022-07-11 14:32:13 +0800

    update history  storage using rlite

--- banditrl/storage/history.py
+++ banditrl/storage/history.py
@@ -3,6 +3,8 @@ History storage
 """
 from abc import abstractmethod
 from datetime import datetime
+import hirlite
+import pickle
 
 
 class History(object):
@@ -17,7 +19,11 @@ class History(object):
     rewarded_at : {datetime, None}
     """
 
-    def __init__(self, history_id, context, recommendations, created_at,
+    def __init__(self,
+                 history_id, 
+                 context, 
+                 recommendations, 
+                 created_at,
                  rewarded_at=None):
         self.history_id = history_id
         self.context = context
@@ -199,3 +205,115 @@ class MemoryHistoryStorage(HistoryStorage):
         history = self.unrewarded_histories.pop(history_id)
         history.update_reward(rewards, rewarded_at)
         self.histories[history.history_id] = history
+
+class RliteHistoryStorage(HistoryStorage):
+    """HistoryStorage that store History objects in redis."""
+
+    def __init__(self,rlite_path=None,model_id=None):
+        if rlite_path is None:
+            self.rlite_client= hirlite.Rlite("online_rlite.db",encoding='utf8')
+        else:
+            self.rlite_client= hirlite.Rlite(rlite_path,encoding='utf8')
+        self._model_id = model_id
+
+    def get_history(self, history_id,model_id=None):
+        """Get the previous context, recommendations and rewards with
+        history_id.
+        Parameters
+        ----------
+        history_id : int
+            The history id of the history record to retrieve.
+        Returns
+        -------
+        history: History
+        Raise
+        -----
+        KeyError
+        """
+        if model_id is None:
+            model_id = self._model_id
+            
+        histories_key = "banditrl:histories:{0}:{1}".format(model_id,history_id)
+        histories = self.rlite_client.command("get",histories_key)
+        if histories is None:
+            return histories
+        return pickle.loads(histories)
+
+    def get_unrewarded_history(self, history_id,model_id=None):
+        """Get the previous unrewarded context, recommendations and rewards with
+        history_id.
+        Parameters
+        ----------
+        history_id : int
+            The history id of the history record to retrieve.
+        Returns
+        -------
+        history: History
+        Raise
+        -----
+        KeyError
+        """
+        if model_id is None:
+            model_id = self._model_id
+        unrewarded_histories_key="banditrl:unrewarded_histories:{0}:{1}".format(model_id,history_id)
+        unrewarded_histories = self.rlite_client.command("get",unrewarded_histories_key)
+        if unrewarded_histories is None:
+            return unrewarded_histories
+        return pickle.loads(unrewarded_histories)
+
+    def add_history(self, 
+                    context,
+                    recommendations,
+                    rewards=None, 
+                    request_id = None,
+                    model_id=None):
+        """Add a history record.
+        Parameters
+        ----------
+        context : {dict of list of float, None}
+        recommendations : {Recommendation, list of Recommendation}
+        rewards : {float, dict of float, None}
+        Raise
+        -----
+        """
+        if model_id is None:
+            model_id = self._model_id
+        created_at = datetime.now()
+
+        if rewards is None:
+            history = History(request_id, context, recommendations, created_at)
+            _history = pickle.dumps(history)
+            unrewarded_histories_key="banditrl:unrewarded_histories:{0}:{1}".format(model_id,request_id)
+            self.rlite_client.command("set",unrewarded_histories_key,_history)
+
+        else:
+            rewarded_at = created_at
+            history = History(request_id, context, recommendations, created_at,
+                              rewards, rewarded_at)
+            _history = pickle.dumps(history)
+            histories_key = "banditrl:histories:{0}:{1}".format(model_id,request_id)
+            self.rlite_client.command("set",histories_key,_history)
+        return request_id
+
+    def add_reward(self, history_id, rewards,model_id=None):
+        """Add reward to a history record.
+        Parameters
+        ----------
+        history_id : int
+            The history id of the history record to retrieve.
+        rewards : {float, dict of float, None}
+        Raise
+        -----
+        """
+        rewarded_at = datetime.now()
+        if model_id is None:
+            model_id = self._model_id
+        unrewarded_histories_key="banditrl:unrewarded_histories:{0}:{1}".format(model_id,history_id)
+        histories_key = "banditrl:histories:{0}:{1}".format(model_id,history_id)
+        unrewarded_histories = self.rlite_client.command("get",unrewarded_histories_key)
+        if unrewarded_histories is not None:
+            history = pickle.loads(unrewarded_histories)
+            history.update_reward(rewards, rewarded_at)
+            self.rlite_client.command("set",histories_key,pickle.dumps(history))
+        else:
+            raise KeyError("该ID尚未曝光")
